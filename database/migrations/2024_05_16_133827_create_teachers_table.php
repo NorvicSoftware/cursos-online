6289484fc312b27e8898d0cdbeb9eb5b69923061
Victor <victor@MBP-de-La.localdomain>
2024-05-17 14:21:05 -0400

    proyecto cursos online

---
+++ database/migrations/2024_05_16_133827_create_teachers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 75);
+            $table->string('profession', 35);
+            $table->string('grade', 35)->nullable();
+            $table->string('phone', 18);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
